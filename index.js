cb80d90b784c765d32d40ee022a4c07dda4a138d
qingwei.li <cinwell.li@gmail.com>
2016-04-27 20:25:55 +0800

    add css loader

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
+var cssLoader = require('./css-loader')
+
 module.exports = function (cooking) {
+  var SOURCE_MAP = cooking.config.sourceMap
+
+  // add loader
   cooking.add('loader.vue', {
     test: /\.vue$/,
     loaders: ['vue']
   })
 
+  // add extension
   cooking.config.resolve.extensions.push('.vue')
+
+  // add vue config
+  if (process.env.NODE_ENV === 'PRODUCTION') {
+    cooking.config.vue = {
+      loaders: cssLoader({
+        sourceMap: SOURCE_MAP ? '#source-map' : false,
+        extract: true
+      })
+    }
+  } else {
+    cooking.config.vue = {
+      loaders: cssLoader()
+    }
+  }
 }
